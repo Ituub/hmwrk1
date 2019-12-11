cfd8f72fe1e87007715de68cd8fc256b183f2554
Ituub <48865955+Ituub@users.noreply.github.com>
2019-12-11 17:36:06 +0500

    Add files via upload

---
+++ Program3.pas
@@ -0,0 +1,18 @@
+﻿uses crt;
+var
+a:array [1..10] of integer;
+i:integer;
+begin
+randomize;
+for i:= 1 to 10 do
+a[i]:= random (21)-10;
+for i:= 1 to 10 do
+write(a[i], ' ');
+for i:= 1 to 10 do
+if a[i] mod 2 = 0 then
+a[i]:= a[i] div 2;
+writeln(' ');
+for i:= 1 to 10 do
+write(a[i], ' ');
+writeln(' ');
+end.
---
+++ Program4.pas
@@ -0,0 +1,21 @@
+﻿uses crt;
+var
+a:array [1..10] of integer;
+max,i,index:integer;
+begin
+max := 0;
+randomize;
+for i:= 1 to 10 do
+a[i]:= random (21)-10;
+for i:= 1 to 10 do
+write(a[i], ' ');
+for i:= 1 to 10 do
+if a[i] > max then
+begin
+max := a[i];
+index := i;
+end;
+writeln(' ');
+writeln('maximum - ', max);
+writeln('index - ', index);
+end.
