552f125f6b0536b89c618bd44c14081079315453
Ituub <48865955+Ituub@users.noreply.github.com>
2020-10-10 01:08:02 +0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,23 @@
+package com.company;
+        import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        if (a / 100 == a % 10){
+            System.out.print("Число "+a+": является палиндромом и ");
+        }
+        else {
+            System.out.print("Число "+a+": не является палиндромом и ");
+        }
+        if (a % 2 == 0){
+            System.out.println("чётным числом, следовательно формула a * b");
+        }
+        else {
+            System.out.println("нечётным числом, следовательно формула a + b");
+        }
+    }
+
+}
